03a4f03198b70bb5b17cc4473b882f255f446f11
Jose <jmejiamu@gmail.com>
2021-07-25 23:34:10 -0700

    styling initial screen

--- App.tsx
+++ App.tsx
@@ -1,23 +1,36 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Dimensions } from 'react-native';
 
+const { width } = Dimensions.get('window');
 const App = () => {
   return (
     <View style={styles.container}>
-      <Text>React Native app with typescript</Text>
-      <StatusBar style="auto" />
-    </View>
+      <View style={styles.bckGroundView}>
+
+        <Text style={{ color: 'white' }}>React Native app with typescript</Text>
+      </View>
+    </View >
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#DFBB3C',
     alignItems: 'center',
     justifyContent: 'center',
+    // borderBottomEndRadius: 500,
   },
+  bckGroundView: {
+    flex: 1,
+    width: '100%',
+    backgroundColor: '#35332C',
+    alignItems: 'center',
+    justifyContent: 'center',
+    borderBottomEndRadius: 500,
+
+  }
 });
 
 export default App;
