c706d418621eeb3e1d3260f96353f166eb3d1dd8
Jose <jmejiamu@gmail.com>
2021-07-25 22:48:45 -0700

    typescript config

--- App.tsx
+++ App.tsx
@@ -2,10 +2,10 @@ import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
-export default function App() {
+const App = () => {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <Text>React Native app with typescript</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -19,3 +19,5 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
 });
+
+export default App;
